70c79ce379ebd7210ae9e731ef035ca351d4697d
YOS G-spec <yos.g.spec@gmail.com>
2023-12-24 16:09:43 +0900

    りふぁくたりんぐ

--- dir2b64.py
+++ dir2b64.py
@@ -17,59 +17,39 @@ class Dir2Base64:
 		self.value = ""
 
 	def zip(self):
-		shutil.make_archive(
-			self.tmp,
-			"zip",
-			self.targetDir
-		)
-		return self
+		shutil.make_archive(self.tmp, "zip", self.targetDir)
 
 	def unZip(self):
-		shutil.unpack_archive(
-			self.tmpZip,
-			self.targetDir
-		)
-		return self
+		shutil.unpack_archive(self.tmpZip, self.targetDir)
+		os.remove(self.tmpZip)
 
 	def base64(self):
 		with open(self.tmpZip, "br") as f:
 			self.value = base64.b64encode(f.read())
-		return self
+		os.remove(self.tmpZip)
 
 	def unBase64(self):
 		with open(self.tmpZip, "bw") as f:
 			f.write(base64.b64decode(self.value.encode()))
-		return self
 
 	def read(self):
 		with open(self.targetB64, 'r') as f:
 			self.value = f.read()
-		return self
 
 	def write(self):
 		with open(self.targetB64,"w") as f:
 			f.write(self.value.decode())
-		return self
 
 	def stdin(self):
 		self.value = input(self.prompt).strip()
-		return self
 
 	def stdout(self):
 		print(self.prompt)
 		print(str(self.value, "utf-8"))
-		return self
-
-	def rmZip(self):
-		try:
-			os.remove(self.tmpZip)
-		finally:
-			return self
 
 def main():
 	parser = argparse.ArgumentParser(
 		description="Compress targetDir to Zip/Base64")
-
 	parser.add_argument("targetDir",
 		help="target directory")
 	parser.add_argument("-e", "--extract",
@@ -82,25 +62,26 @@ def main():
 	args = parser.parse_args()
 
 	d2b = Dir2Base64(args.targetDir)
-	try:
-		if args.extract:
-			if args.outputFile:
-				d2b.read(),
-			else:
-				d2b.stdin()
-			d2b.unBase64().unZip()
-			print("Extracted!")
 
+	if args.extract:
+		if args.outputFile:
+			d2b.read()
 		else:
-			d2b.zip().base64()
-			if args.outputFile:
-				d2b.write()
-				print("Compressed!")
-			else:
-				d2b.stdout()
+			d2b.stdin()
 
-	finally:
-		d2b.rmZip()
+		d2b.unBase64()
+		d2b.unZip()
+		print("Extracted!")
+
+	else:
+		d2b.zip()
+		d2b.base64()
+
+		if args.outputFile:
+			d2b.write()
+			print("Compressed!")
+		else:
+			d2b.stdout()
 
 if __name__ == "__main__":
 	main()
